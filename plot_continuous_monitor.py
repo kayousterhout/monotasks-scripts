e059ead587d5ae19a5767e137ff0e70481b33372
Christopher Canel <christopher.canel@icloud.com>
2015-10-07 12:33:26 -0700

    Check before extracting "Running Disk Monotasks".
    This commit modifies plot_continuous_monitor.py to check if the "Running
    Disk Monotasks" key is in the JSON data before trying to extract the
    corresponding value. This fixes a bug where plot_continuous_monitor.py
    could not plot logs captured from Spark because Spark does not log
    "Running Disk Monotasks" (so a key not found error would occur).

--- plot_continuous_monitor.py
+++ plot_continuous_monitor.py
@@ -77,16 +77,17 @@ def plot_continuous_monitor(filename, open_graphs=False):
     running_compute_monotasks = 0
     if "Running Compute Monotasks" in json_data:
       running_compute_monotasks = json_data["Running Compute Monotasks"]
+    xvdf_running_disk_monotasks = 0
+    xvdb_running_disk_monotasks = 0
+    if "Running Disk Monotasks" in json_data:
+      # Parse the number of currently running disk monotasks for each disk. xvdf is mounted on /mnt2
+      # and xvdb on /mnt (the indexing below will need to be updated if we use different instance
+      # types).
+      running_disk_monotasks_info = json_data["Running Disk Monotasks"]
+      RUNNING_MONOTASKS_KEY = "Running And Queued Monotasks"
+      xvdf_running_disk_monotasks = running_disk_monotasks_info[0][RUNNING_MONOTASKS_KEY]
+      xvdb_running_disk_monotasks = running_disk_monotasks_info[1][RUNNING_MONOTASKS_KEY]
     running_macrotasks = 0
-
-    # Parse the number of currently running disk monotasks for each disk.
-    # xvdf is mounted on /mnt2 and xvdb on /mnt (the indexing below will need to be updated
-    # if we use different instance types).
-    running_disk_monotasks_info = json_data["Running Disk Monotasks"]
-    RUNNING_MONOTASKS_KEY = "Running And Queued Monotasks"
-    xvdf_running_disk_monotasks = running_disk_monotasks_info[0][RUNNING_MONOTASKS_KEY]
-    xvdb_running_disk_monotasks = running_disk_monotasks_info[1][RUNNING_MONOTASKS_KEY]
-
     if "Running Macrotasks" in json_data:
       running_macrotasks = json_data["Running Macrotasks"]
     gc_fraction = 0
